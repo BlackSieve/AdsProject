1c11006bc6684eca6d0d4da3ba407a45d6604872
BlackSieve <ivansarinov25@gmail.com>
2024-06-26 20:40:57 +0300

    News

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -7,6 +7,9 @@ from django.db.models.functions import Coalesce
 class Category (models.Model):
     name = models.CharField(max_length=50, unique=True)
 
+    def __str__(self):
+        return self.name.title()
+
 
 class Author(models.Model):
 
@@ -54,6 +57,9 @@ class Post (models.Model):
     def dislike(self):
         self.rating -= 1
         self.save()
+
+    def __str__(self):
+        return f'{self.news.title():{self.text[:20]}}'
 
 
 class PostCategory (models.Model):
