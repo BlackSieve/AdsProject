7c5120d0c3f28728e4779ebdaf6fd0e7738cec85
BlackSieve <ivansarinov25@gmail.com>
2024-06-27 20:54:44 +0300

    News

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
 
 
 class PostList(ListView):
@@ -8,6 +8,11 @@ class PostList(ListView):
     ordering = 'news'
     template_name = 'news.html'
     context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now']=datetime.utcnow()
+        context['next_sale']=None
 
 
 class PostDetail(DetailView):
