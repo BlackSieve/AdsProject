9ed94f5c7e89b54e455ee6f23eb77b8a6c86681b
BlackSieve <ivansarinov25@gmail.com>
2024-07-02 19:34:11 +0300

    News

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
 from datetime import datetime
+from .filters import PostFilter
 
 
 class PostList(ListView):
@@ -8,11 +9,18 @@ class PostList(ListView):
     ordering = '-date'
     template_name = 'news.html'
     context_object_name = 'news'
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time_now']=datetime.utcnow()
         context['next_sale']=None
+        context['filterset']=self.filterset
         return context
 
 
