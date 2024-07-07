9b73305929ce5a517da66db2617ac1a0afeb2f40
BlackSieve <ivansarinov25@gmail.com>
2024-07-07 12:13:35 +0300

    News

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,11 +1,17 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
 
 
 from .models import Post
 from datetime import datetime
 from .filters import PostFilter
 from .forms import PostForms
+from  django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class PostAuth(LoginRequiredMixin, TemplateView):
+    form_class = PostForms
+    model = Post
 
 
 class PostList(ListView):
@@ -47,13 +53,13 @@ class PostCreate(CreateView):
         return super().form_valid(form)
 
 
-class PostUpdate(UpdateView):
+class PostUpdate(LoginRequiredMixin, UpdateView):
     form_class = PostForms
     model = Post
     template_name = 'post_edit.html'
 
 
-class PostDelete(DeleteView):
+class PostDelete(LoginRequiredMixin,DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('news')
